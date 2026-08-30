44cabeb9384a1be792acd2fcb586f0522b70f25a
Manuel Quarneti <mq1@ik.me>
2026-08-30 14:34:07 +0200

    Update .config/nvim/lua/plugins/blink-cmp.lua Update .config/nvim/lua/plugins/mini.lua

--- dot_config/nvim/lua/plugins/blink-cmp.lua
+++ dot_config/nvim/lua/plugins/blink-cmp.lua
@@ -2,5 +2,6 @@ return {
 	"saghen/blink.cmp",
 	dependencies = { "rafamadriz/friendly-snippets" },
 	version = vim.version.range("1.*"),
+	priority = 25,
 	opts = {},
 }
--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -1,5 +1,7 @@
 return {
 	"nvim-mini/mini.nvim",
+	priority = 100,
+	lazy = false, -- required to enable mini.files early
 	config = function()
 		require("mini.icons").setup()
 		require("mini.notify").setup()
