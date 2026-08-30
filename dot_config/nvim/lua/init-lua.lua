16eb00091a1b64ad0123d8bce42e4620179d6569
Manuel Quarneti <mq1@ik.me>
2026-08-30 13:22:58 +0200

    Remove .config/nvim/lua/init-lua.lua Remove .config/nvim/lua/init-rust.lua Remove .config/nvim/lua/init-typst.lua

--- dot_config/nvim/lua/init-lua.lua
+++
@@ -1,6 +0,0 @@
--- lua integration -------------------------------------------------------------
-
-vim.lsp.enable({ "lua_ls" })
-
--- formatting
-require("conform").formatters_by_ft.lua = { "stylua" }
