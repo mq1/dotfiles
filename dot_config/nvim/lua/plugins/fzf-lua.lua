4c1d63406a04f5c63e5ceed69fc22dc0e5c43295
Manuel Quarneti <mq1@ik.me>
2026-09-09 22:53:12 +0200

    Update .config/nvim/lua/plugins/fzf-lua.lua

--- dot_config/nvim/lua/plugins/fzf-lua.lua
+++ dot_config/nvim/lua/plugins/fzf-lua.lua
@@ -2,7 +2,7 @@ return {
 	src = "https://github.com/ibhagwan/fzf-lua",
 	defer = true,
 	config = function()
-		require("fzf-lua").setup()
+		require("fzf-lua").setup({ "skim" })
 
 		vim.keymap.set("n", "<Leader>f", "<Cmd>FzfLua files<CR>", { desc = "Find file", silent = true })
 		vim.keymap.set("n", "<Leader>b", "<Cmd>FzfLua buffers<CR>", { desc = "Find buffer", silent = true })
