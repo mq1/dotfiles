3057de201c1a4ba40594413df51720e062335c05
Manuel Quarneti <mq1@ik.me>
2026-09-09 14:21:13 +0200

    Update .config/nvim/init.lua Update .config/nvim/lua/plugins/grug-far.lua

--- dot_config/nvim/lua/plugins/grug-far.lua
+++ dot_config/nvim/lua/plugins/grug-far.lua
@@ -3,5 +3,13 @@ return {
 	defer = true,
 	config = function()
 		require("grug-far").setup()
+
+		vim.keymap.set("n", "<Leader>sr", "<Cmd>GrugFar<CR>", { desc = "Search & replace", silent = true })
+		vim.keymap.set(
+			"n",
+			"<Leader>sf",
+			"<Cmd>GrugFarWithin<CR>",
+			{ desc = "Search & replace (current file)", silent = true }
+		)
 	end,
 }
