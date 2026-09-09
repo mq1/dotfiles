5d71ede7a37005058fac3ae2c5a86ca8c02cf409
Manuel Quarneti <mq1@ik.me>
2026-09-09 14:21:55 +0200

    Add .config/nvim/lua/plugins/fzf-lua.lua

---
+++ dot_config/nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,11 @@
+return {
+	src = "https://github.com/ibhagwan/fzf-lua",
+	defer = true,
+	config = function()
+		require("fzf-lua").setup({ { "fzf-native", "hide" } })
+
+		vim.keymap.set("n", "<Leader>ff", "<Cmd>FzfLua files<CR>", { desc = "Find file", silent = true })
+		vim.keymap.set("n", "<Leader>fb", "<Cmd>FzfLua buffers<CR>", { desc = "Find buffer", silent = true })
+		vim.keymap.set("n", "<Leader>fg", "<Cmd>FzfLua live_grep_native<CR>", { desc = "Find text", silent = true })
+	end,
+}
