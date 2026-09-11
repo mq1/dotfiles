bbbe16b6f94e996bccd74601a91a2f772d5dd34f
Manuel Quarneti <mq1@ik.me>
2026-09-11 23:52:23 +0200

    Remove .config/nvim/lua/plugins/tiny-inline-diagnostic.lua

--- dot_config/nvim/lua/plugins/tiny-inline-diagnostic.lua
+++
@@ -1,7 +0,0 @@
-return {
-	src = "https://github.com/rachartier/tiny-inline-diagnostic.nvim",
-	defer = true,
-	config = function()
-		require("tiny-inline-diagnostic").setup({ preset = "powerline" })
-	end,
-}
