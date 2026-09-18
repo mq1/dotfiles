d1580c3530215b56c51d10dac4d9e14cab5ee82a
Manuel Quarneti <mq1@ik.me>
2026-09-18 21:21:53 +0200

    Update .config/nvim/lua/plugins/fzf-lua.lua

--- dot_config/nvim/lua/plugins/fzf-lua.lua
+++ dot_config/nvim/lua/plugins/fzf-lua.lua
@@ -4,8 +4,20 @@ return {
 	config = function()
 		require("fzf-lua").setup({ "skim" })
 
-		vim.keymap.set("n", "<Leader>f", "<Cmd>FzfLua files<CR>", { desc = "Find file", silent = true })
-		vim.keymap.set("n", "<Leader>b", "<Cmd>FzfLua buffers<CR>", { desc = "Find buffer", silent = true })
-		vim.keymap.set("n", "<Leader>g", "<Cmd>FzfLua live_grep_native<CR>", { desc = "Find text", silent = true })
+		vim.keymap.set("n", "<Leader>f", function()
+			FzfLua.files()
+		end, { desc = "Find file", silent = true })
+
+		vim.keymap.set("n", "<Leader>b", function()
+			FzfLua.buffers()
+		end, { desc = "Find buffer", silent = true })
+
+		vim.keymap.set("n", "<Leader>g", function()
+			FzfLua.live_grep_native()
+		end, { desc = "Find text", silent = true })
+
+		vim.keymap.set("n", "<Leader>d", function()
+			FzfLua.diagnostics_workspace()
+		end, { desc = "Diagnostics", silent = true })
 	end,
 }
